a5b9a20580485e5562acd973718bd8436592a8b9
John Romano <jromano269962@gmail.com>
2016-10-24 17:31:11 +0800

    Updated Some Text

--- description_info.hpp
+++ description_info.hpp
@@ -9,12 +9,12 @@
 author = "Romano";
 
 // Mission name
-briefingName = "Bonyo+: Chernarus"; // in mission select menu
-onLoadName = "Bonyo+: Chernarus"; // on load screen
+briefingName = "Bonyo+ Chernarus"; // in mission select menu
+onLoadName = "Bonyo+ Chernarus"; // on load screen
 
 // Picture shown
 //loadScreen = "Images\orbat.paa"; // on load screen
 
 // Mission description
 overviewText = "Slay wave after wave of enemies to help facilitate your looting addiction."; // in mission select menu
-onLoadMission = "Made by J.Romano"; // on load screen
+onLoadMission = "Property of Unity Security Force"; // on load screen
